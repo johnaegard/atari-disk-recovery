ccb93f8300bb5ab7287dbe8630dff0da348a3bda
johnzo <johnzo@johnzo.com>
2021-11-14 01:25:42 -0800

    progress

--- gfa-basic/sectors/read-gfa.py
+++ gfa-basic/sectors/read-gfa.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
 
 import sys
-import glob
-import subprocess 
-
-class PointerJumpedTooFarError(Exception):
-  pass
-
-class IllegalOpError(Exception):
-  pass
 
 identifiers = []
-indent =0
-sep = []
-num_ids = []
 
-default_op = { "name" : "????", "args": lambda x: "", "openindent":False, "closeindent":False}
-id_classes = ['#','$','%','!','#[]','$[]','%[]','![]','&','|','10',' procedure','&[]','|[]']
-suffixes   = ['#','$','%','!','#[]','$[]','%[]','![]','&','|','10','','&[]','|[]']
-MAX_OVERSHOOT=40
-MAX_GFA_OPERATION=1695
 def gosub_parms(bites):
   bites_hex = '{:02x}'.format(bites[0]) + '{:02x}'.format(bites[1])
   proc_idx = int(bites_hex,16)
@@ -68,13 +52,12 @@ def let_bool_ary_parms(bites):
 def let_parms(id_type, bites):
   bites_hex = '{:02x}'.format(bites[0]) + '{:02x}'.format(bites[1])
   var_idx = int(bites_hex,16)
-  #print("{:d} {:04x} {:s}".format(id_type,var_idx))
+  #print("****{:d} {:04x}".format(id_type,var_idx))
   #identifier = identifiers[id_type][var_idx] 
   #print("{:d} {:04x} {:s}".format(id_type,var_idx,identifier))
-  try:
-    return identifiers[id_type][var_idx] + " " +dump_parms(bites[2:])
-  except IndexError:
-    raise Exception("identifier out-of-range")
+  return identifiers[id_type][var_idx] + " " +dump_parms(bites[2:])
+  
+default_op = { "name" : "????", "args": lambda x: "", "openindent":False, "closeindent":False}
 
 gfa_ops = {
   0   :  { "name": "DO",              "args": no_parms,    "openindent":True,  "closeindent":False},
@@ -153,158 +136,74 @@ gfa_ops = {
   1588 : { "name": "ARRAYFILL",      "args": dump_parms,  "openindent":False, "closeindent":False}
 }
 
-def read_32bit_uint_bigendian(bites):
+def read_32bit_uint_bigendian(f):
+  bites = f.read(4)
   bites_hex = '{:02x}'.format(bites[0]) + '{:02x}'.format(bites[1]) + '{:02x}'.format(bites[2]) + '{:02x}'.format(bites[3]) 
   return int(bites_hex,16)
 
-def read_16bit_uint_bigendian(bites):
+def read_16bit_uint_bigendian(f):
+  bites = f.read(2)
   bites_hex = '{:02x}'.format(bites[0]) + '{:02x}'.format(bites[1])
   return int(bites_hex,16)
 
-def read_line(bites,pointer):
-  global indent
-  length = read_16bit_uint_bigendian(bites[pointer:pointer+2])
-  if length == 0:
-    raise Exception("zero-length operator!")
-  pointer += 2
-  op = read_16bit_uint_bigendian(bites[pointer:pointer+2])
-  if op > MAX_GFA_OPERATION:
-    raise IllegalOpError("{:d} is not a legal gfa operation!".format(op)) 
-  elif op % 4 != 0:
-    raise IllegalOpError("{:d} not legal gfa op, not divisible by 4!".format(op)) 
-  pointer += 2
-  remainder = bites[pointer:pointer+length-4]
-  pointer += length -4
+indent =0
 
-  # verify RETURN in right place
-  if op == 28 and indent != 1:
-    raise Exception("badly-placed RETURN")
-  #sys.stdout.write("{:04x}".format(length))
-  #sys.stdout.write(" ")
+def read_line(f):
+  global indent
+  sys.stdout.write("{:04x} ".format(f.tell()))
+  length = read_16bit_uint_bigendian(f)
+  op = read_16bit_uint_bigendian(f)
+  remainder = f.read(length - 4)
+  sys.stdout.write("{:04x} ".format(length))
   hex_opcode = "{:04x}".format(op)
-  #sys.stdout.write(hex_opcode + "/")
-  #sys.stdout.write("{:04d}".format(op)+" ")
+  sys.stdout.write(hex_opcode + "/")
+  sys.stdout.write("{:04d}".format(op)+" ")
   opstruct = gfa_ops.get(op,default_op)
   if opstruct["closeindent"]:
     indent -=1
-  #for c in range(indent):
-  #  sys.stdout.write("  ")
-  #sys.stdout.write(opstruct.get("name") + " ")
-  #sys.stdout.write(opstruct.get("args")(remainder))
-  opstruct.get("args")(remainder)
-  #print()
+  for c in range(indent):
+    sys.stdout.write("  ")
+  sys.stdout.write(opstruct.get("name") + " ")
+  sys.stdout.write(opstruct.get("args")(remainder))
+  print()
   if opstruct["openindent"]:
     indent +=1
 
-  return pointer
+id_classes = ['#','$','%','!','#[]','$[]','%[]','![]','&','|','10',' procedure','&[]','|[]']
+suffixes   = ['#','$','%','!','#[]','$[]','%[]','![]','&','|','10','','&[]','|[]']
 
-def read_file(bites):
-  global identifiers
-  global sep
-  global num_ids
-  global indent
-  identifiers = []
-  sep = []
-  num_ids = []
-  indent = 0
-  pointer = 12 # 2 bytes of version + 10 bytes of magic number
+sep = []
+num_ids = []
+
+with open("/tmp/foo", "rb") as f:
+#with open("PROC.GFA", "rb") as f:
+  f.read(2)  # 2 bytes of version
+  f.read(10) # 10 bytes of magic number
   for i in range(38):
-    sep.append(read_32bit_uint_bigendian(bites[pointer:pointer+4]))
-    pointer+=4
+    sep.append(read_32bit_uint_bigendian(f))
 
-  #for i in range(38):
-  #  print('{:d} {:08x}'.format(i,sep[i]))
+  for i in range(38):
+    print('{:d} {:08x}'.format(i,sep[i]))
 
-  #print("identifier block is {:d} bytes long".format(sep[16] - sep[0]))
-  # print("code is {:d} bytes long".format(sep[19] - sep[16]))
+  print("identifier block is {:d} bytes long".format(sep[16] - sep[0]))
+  print("code is {:d} bytes long".format(sep[19] - sep[16]))
 
   for i in range(14):
     num_ids.append(int((sep[20+i] - sep[19+i]) / 4))
 
   for id_class in range(14):
     identifiers.append([])
-    # print("{:d} {:s} IDENTIFIERS".format(num_ids[id_class],id_classes[id_class]))
+    print("{:d} {:s} IDENTIFIERS".format(num_ids[id_class],id_classes[id_class]))
     id_counter=0
     while id_counter < num_ids[id_class]:
-      id_length = int(bites[pointer])
-      pointer += 1
+      id_length = int(f.read(1)[0])
       if id_length ==0:
         continue
-      identifiers[id_class].append(bites[pointer:pointer+id_length].decode("utf-8").lower() + suffixes[id_class])
-      pointer += id_length
+      identifiers[id_class].append(f.read(id_length).decode("utf-8").lower() + suffixes[id_class])
       # print("  {:x} {:s}".format(id_counter,identifiers[id_class][id_counter]))
       id_counter+=1 
 
-  pointer += 6   # miscellaneous debris at end of id section. 
+  f.read(6)   # the bits between identifiers and program.
 
   while(True):
-    try:
-      newpointer = read_line(bites,pointer) 
-      if newpointer > len(bites):
-        raise PointerJumpedTooFarError("pointer starting at {:04x} jumped {:04x} beyond EOF".format(pointer,newpointer))
-      pointer = newpointer
-    except PointerJumpedTooFarError as err:
-      sys.stdout.write("..stopped at byte {:04x}/{:04x}..{:s} {:s}..".format(newpointer,len(bites),str(type(err).__name__),str(err)))
-      if newpointer - len(bites) > MAX_OVERSHOOT:
-        sys.stdout.write("overshot by > {:d} bytes..".format(MAX_OVERSHOOT))
-        return False
-      else:
-        return True
-    except IndexError as err:
-      sys.stdout.write("..stopped at byte {:04x}/{:04x}..{:s} {:s}..".format(newpointer,len(bites),str(type(err).__name__),str(err)))
-      if len(bites) - pointer > MAX_OVERSHOOT:
-        sys.stdout.write("undershot EOF by {:d} bytes..".format(pointer,MAX_OVERSHOOT))
-        return False
-      else: 
-        return True
-    except IllegalOpError as err:
-      sys.stdout.write("..stopped at byte {:04x}/{:04x}..{:s} {:s}..".format(newpointer,len(bites),str(type(err).__name__),str(err)))
-      return False
-    except BaseException as err:
-      sys.stdout.write("..stopped at byte {:04x}/{:04x}..{:s} {:s}..".format(newpointer,len(bites),str(type(err).__name__),str(err)))
-      if str(err) == "badly-placed RETURN":
-        return False
-      if str(err) == "identifier out-of-range":
-        return False 
-      elif str(err) == "zero-length operator!":
-        return False
-      return True
-
-def sector_matches(sequence,candidate):
-  cat = ["cat"] + sequence + [candidate]
-  catcall = subprocess.run(cat, text=False, capture_output=True)
-  return read_file(catcall.stdout)
-
-def advance_one_sector(cycle,idx,num_sequences,sequence):
-  sequences = []
-  for sector in sorted( glob.glob("*.sec"), key=lambda x: int(x.split('.')[0]) ):
-    if sector in sequence:
-      continue
-    sys.stdout.write("cycle {:d}..seq {:d}/{:d}: testing sector {:s}".format(cycle,idx,num_sequences,sector))
-    if sector_matches(sequence,sector):
-      sys.stdout.write("OK")
-      candidate_sequence = sequence.copy()
-      candidate_sequence.append(sector)
-      sequences.append(candidate_sequence)
-    else:    
-      sys.stdout.write("BAD")
-    print()
-  return sequences
-
-if __name__ == '__main__':
-  sequences  = [["1242.sec","1243.sec","1244.sec","1245.sec","1246.sec","1247.sec","1248.sec"]]
-  running = True
-  advanced_sequences = [] 
-  cycle = 0 
-  while running:
-    cycle += 1
-    for i,s in enumerate(sequences):
-      advanced_sequences = advanced_sequences + advance_one_sector(cycle,i,len(sequences),s)
-    print("Cycle {:d} ... {:d} sequences".format(cycle,len(advanced_sequences)))
-    if cycle ==1:
-    #if len(advanced_sequences) == 0:
-      running = False
-    sequences = advanced_sequences
-
-  for seq in sequences:
-    print("cat " + " ".join(seq) + " > /tmp/foo; ./read-gfa.py")
+    read_line(f) 
